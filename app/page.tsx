dbb36a89ffe1313d20ccd5db9780d999578baed9
apple <apple@apple-iMac-001.local>
2026-04-23 09:31:57 +0900

    파일 삭제

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { ShoppingLogin } from "@/components/shopping-login";
-
-export default function Home() {
-  return (
-    <main className="flex min-h-screen items-start justify-center py-8">
-      <ShoppingLogin />
-    </main>
-  );
-}
